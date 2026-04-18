b72c72e7fac32704c92be17321e9648845a59f55
SundarAbbas <chughtaiabbas486@gmail.com>
2026-04-18 17:41:41 +0500

    Deploy Settings

--- core/settings.py
+++ core/settings.py
@@ -10,7 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
-from pathlib import Path
+import os
+from pathlib import Path 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-5^akk@nj*nzi55@o!2f-w+rx9%ia^vj)0hni3hv6+kjk#cxv4x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -43,6 +44,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'rest_framework',
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",     # Your Vite React port
     "http://127.0.0.1:5173",
